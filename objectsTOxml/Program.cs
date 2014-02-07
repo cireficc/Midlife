b5e49e50cbc40177bf4aa58b55d8e8d62351b1a6
Dan <radiospace83@gmail.com>
2014-02-06 17:21:17 -0800

    serialize a class to xml
    i have provided an example project to the solution. for creating an xml
    file from a class

---
+++ objectsTOxml/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace objectsTOxml
+{
+    
+    class Program
+    {
+
+        //this attribute is so we can call SaveFileDialog
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string word = "";
+            string def = "";
+            Console.Write("type a word: ");
+            word = Console.ReadLine();
+
+            Console.Write("type the definition: ");
+            def = Console.ReadLine();
+
+            Console.Write("Are you ready to save an xml file? (y or n): ");
+            if (Console.ReadLine().ToUpper() == "Y")
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "XML|*.xml";
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                { //save the file
+
+                    XmlSerializer xml = new XmlSerializer(typeof(WordLite));
+                    WordLite wl = new WordLite(word, def);
+
+                    using (FileStream fs = new FileStream(sfd.FileName, FileMode.Create, FileAccess.Write))
+                    {//create the xml file
+                        xml.Serialize(fs, wl);
+                    }
+
+                    Console.WriteLine("YOU have made an xml file check it out at " + sfd.FileName);
+                }
+                else 
+                {
+                    Console.WriteLine("Please leave. You have hurt my feelings.\r\n All I did was try to make a simple xml file for you \r\n and you just quit. THANKS!");                    
+                }
+
+                Console.ReadLine();
+
+            }
+
+            
+
+
+
+        }
+    }
+}
---
+++ objectsTOxml/WordLite.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace objectsTOxml
+{
+
+    //needs to be public
+    public class WordLite
+    {
+        /*
+         NOTE:  for the word and definition feilds if you do not want a public setter
+         * you have to use IXmlSerializable interface and implement it 
+         * 
+         * which is really not that difficult. and with a complex class like Word we may want to 
+         * so we can optimize it.
+         
+         */
+
+
+        string word;
+        /// <summary>
+        /// the text of the word
+        /// </summary>
+        public string Word { get { return word; } set { word = value; } }
+
+        string definition;
+        /// <summary>
+        /// the definitino of the word
+        /// </summary>
+        public string Definition { get { return definition; } set { definition = value; } }
+        
+        /// <summary>
+        /// creates a light version of midlife's Word class
+        /// </summary>
+        /// <param name="w">the word to define</param>
+        /// <param name="d">the definition of the word</param>
+        public WordLite(string w,string d)
+        {
+            word = w;
+            definition = d;
+        }
+
+        //needs a parameterless constructor for serialization
+        public WordLite()
+        {
+            word = "";
+            definition = "";
+        }
+    }
+}
