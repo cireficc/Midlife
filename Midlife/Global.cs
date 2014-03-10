7579fa712dc93b0618d5952bffe2019a7e57d971
Christopher Cirefice <cireficc@mail.gvsu.edu>
2014-03-10 23:52:06 +0100

    Getting into building the Grammar Globals section. This is kind of important so that we have a better layout for the XML (because I now realize it's only 75% complete), as well as the deserialization (because we want to save space when we have things in memory; ergo, enums galore to represent the grammar).

--- Midlife/Global.cs
+++ Midlife/Global.cs
@@ -12,22 +12,91 @@ namespace Language
         {
             /*
              * This list is for displaying conjugations correctly. It will also be used in grammatical analysis,
-             * to see if the user formed the conjugation correctly. Needs to be extended to include accented vowels.
+             * to see if the user formed the conjugation correctly. Needs to be extended to include vowels from
+             * other languages/character sets (for proper nouns, particularly).
              */
             public static readonly char[] vowels = { 'a', 'à', 'á', 'â', 'ä', 'å', 'æ', 'À', 'Á', 'Â', 'Ã', 'Æ', 'e',
                                                      'ê', 'é', 'ë', 'è', 'E', 'Ê', 'Ë', 'É', 'È', 'i', 'ï', 'í', 'î',
                                                      'ì', 'I', 'Ï', 'Í', 'Ì', 'Î', 'o', 'ô', 'ö', 'ò', 'õ', 'ó', 'ø',
                                                      'œ', 'O', 'Ó', 'Ô', 'Õ', 'Ö', 'Ò', 'Ø', 'Œ', 'u', 'ú', 'ü', 'û',
                                                      'ù', 'U', 'Ù', 'Ú', 'Ü', 'Û', 'y', 'ÿ', 'ý', 'Y', 'Ÿ', 'Ý' };
+
+            enum GrammarForm
+            {
+                Adjective,
+                Adverb,
+                Noun,
+                Preposition,
+                Pronoun,
+                Verb
+            }
         }
-    }
-    // Can there be same class names in different namespaces? That's what namespaces are for... right?
-    // If so, this is how I'll organize globals - each "type" in their own namespaces, for a specific purpose.
-    namespace VerbMaps
-    {
-        class Global
+        namespace Adjective
         {
-            public static string cool = "Cool!";
+            enum Gender
+            {
+                Masculine,
+                Feminine,
+                MasculineFeminine
+            }
+
+            enum Number
+            {
+                Singular,
+                Plural
+            }
+
+            enum Location
+            {
+                Before,
+                After,
+                Both
+            }
+        }
+        namespace Adverb
+        {
+            
+        }
+        namespace Noun
+        {
+            enum Gender
+            {
+                Masculine,
+                Feminine,
+                MasculineFeminine
+            }
+
+            enum Number
+            {
+                Singular,
+                Plural
+            }
+        }
+        namespace Preposition
+        {
+            // Stuff
+        }
+        namespace Pronoun
+        {
+            enum PronounType
+            {
+                Subject,
+                Reflexive,
+                DirectObject,
+                IndirectObject,
+                Disjunctive
+            }
+
+            enum PronounReplaces
+            {
+                DefiniteDirectObject,
+                IndirectObject,
+                IndefiniteDirectObject
+            }
+        }
+        namespace Verb
+        {
+            // Adding stuff later
         }
     }
 }
